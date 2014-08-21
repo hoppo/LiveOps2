5a1c2b43502b93fa6fc6a4c268dcb81819b50d54
johnhopkins <johnhopkins@gmx.co.uk>
2014-08-21 11:47:32 +0100

    Added root index page

--- LiveOperations_project/urls.py
+++ LiveOperations_project/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+from LiveOperations_project.views import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'LiveOperations_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', index), 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^IMGGaming/', include('IMGGaming.urls')),
 )
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@ TODO
 ++++
 
 Tidy up the Add report page for IMG Gaming.
-Create index for Live Operations
+Improve Live Operations root index page
 Move base template into Live Operations
 Inherit IMG Gaming base template from Live Operations
 Add a search function to search reports for IMG Gaming

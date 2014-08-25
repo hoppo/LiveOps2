c67e3aca6a2ef37f103da3e08daad1b5f2d9600b
johnhopkins <johnhopkins@gmx.co.uk>
2014-08-25 22:01:16 +0100

    Improvement to Live Operations Index.html

--- LiveOperations_project/views.py
+++ LiveOperations_project/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 
 def index(request):
-    return HttpResponse("Root index for Live Operations")
+    context = RequestContext(request)
+    return render_to_response('LiveOperations/index.html', {}, context)

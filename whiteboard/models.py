92467589c2925d9896f1becab99efde6445aaab4
johnhopkins <johnhopkins@gmx.co.uk>
2014-08-28 19:41:11 +0100

    Better Whiteboard System

--- whiteboard/models.py
+++ whiteboard/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Match(models.Model):
-    name = models.CharField(max_length=50, unique=True,null=True,blank=True)
+    name = models.CharField(max_length=50, null=True,blank=True)
     bt = models.BooleanField(blank=True)
     adi = models.BooleanField(blank=True)
-    isdn = models.CharField(max_length=12, unique=True,null=True,blank=True)
+    isdn = models.CharField(max_length=12, null=True,blank=True)
     sound_eng = models.CharField(max_length=12,null=True,blank=True)
     gallery = models.IntegerField(null=True,blank=True)
 

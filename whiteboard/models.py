e7eeec9a390c478e1cdee3e8495cd3269ca1f7d8
johnhopkins <johnhopkins@gmx.co.uk>
2014-09-09 14:34:47 +0100

    tweaked for django 1.7 booleanFields in model

--- whiteboard/models.py
+++ whiteboard/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Match(models.Model):
     name = models.CharField(max_length=50, null=True,blank=True)
-    bt = models.BooleanField(blank=True)
-    adi = models.BooleanField(blank=True)
-    gal = models.BooleanField(blank=True)
-    mars = models.BooleanField(blank=True)
-    swif = models.BooleanField(blank=True)
-    dialled = models.BooleanField(blank=True)
+    bt = models.BooleanField(default=False,blank=True)
+    adi = models.BooleanField(default=False,blank=True)
+    gal = models.BooleanField(default=False,blank=True)
+    mars = models.BooleanField(default=False,blank=True)
+    swif = models.BooleanField(default=False,blank=True)
+    dialled = models.BooleanField(default=False,blank=True)
     isdn = models.CharField(max_length=12,null=True,blank=True)
     codec = models.CharField(max_length=3,null=True,blank=True)
     sound_eng = models.CharField(max_length=12,null=True,blank=True)
